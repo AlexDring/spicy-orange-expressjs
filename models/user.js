1899f567830ae7b7afa3ec8c15a19f8915122b9f
Alex Dring <alexdringppc@gmail.com>
2021-05-28 12:06:29 +0100

    add user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passwordHash: String
+})
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
