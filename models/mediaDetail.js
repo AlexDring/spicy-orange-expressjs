e0306e0c060ef4618cbae3e9d8a782626eb63244
Alex Dring <alexdringppc@gmail.com>
2021-06-01 14:32:49 +0100

    add media and mediaDeail models

---
+++ models/mediaDetail.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const mediaDetailSchema = new mongoose.Schema({
+  media: { type: mongoose.Schema.Types.ObjectId, ref: 'Media' },
+  Rated: String,
+  Released: String,
+  Writer: String,
+  Actors: String,
+  Plot: String,
+  Country: String,
+  Awards: String,
+  Ratings: [{ Source: String, Value: String }],
+  imdbVotes: String,
+  imdbID: {
+    type: String,
+    unique: true
+  },
+  DVD: String,
+  BoxOffice: String,
+  Production: String,
+  Website: String,
+  Response: String,
+  dateAdded: String,
+  rottenReviews: [{ score: Number, user: String, review: String }],
+  userId: String
+})
+
+mediaDetailSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('MediaDetail', mediaDetailSchema)
